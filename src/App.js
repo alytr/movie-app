053340b67317ce15e4420953579b15179565c854
alytr <alygomyco1@gmail.com>
2022-02-28 13:12:19 +0000

    a jour

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import MovieList from './components/MovieList';
-import MovieListHeading from './components/MovieListHeading';
-import SearchBox from './components/SearchBox';
+import MovieList from "./component/MovieList";
+import MovieListHeading from "./component/MovieListHeading";
+import SearchBox from "./component/SearchBox";
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
-	const [searchValue, setSearchValue] = useState('');	
+	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
 		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
